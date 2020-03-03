d30d13ea30c0d0b38f231a2c034b0020bd486c59
YANNI <yanniflambas@hotmail.com>
2020-03-03 21:07:13 +1100

    Updates

---
+++ precache-manifest.933660a717c8e9bc87fca8f413501510.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d7bdfcc858ef31efe8f1f4a9260e6f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4cfd6b0cfa4b25143934",
+    "url": "/portfolio/static/css/main.2f2ae527.chunk.css"
+  },
+  {
+    "revision": "e29c84e9d5850ce666ed",
+    "url": "/portfolio/static/js/2.02506777.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.02506777.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cfd6b0cfa4b25143934",
+    "url": "/portfolio/static/js/main.2d7579fb.chunk.js"
+  },
+  {
+    "revision": "1a95985286676740b556",
+    "url": "/portfolio/static/js/runtime-main.f6eb50f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
